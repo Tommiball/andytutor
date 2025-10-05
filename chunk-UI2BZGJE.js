5eb31706ea10338c6dcf6c3bf8c05c0199d0abc2
Michael Schneider <skylet@quantentunnel.de>
2025-10-05 15:37:16 +0200

    Deploy

---
+++ chunk-UI2BZGJE.js
@@ -0,0 +1,1 @@
+import{$ as n,fb as t,gb as a,tb as i,xb as p}from"./chunk-5F73BLFA.js";var s=class o{static \u0275fac=function(e){return new(e||o)};static \u0275cmp=n({type:o,selectors:[["app-booking-page"]],standalone:!0,features:[p],decls:2,vars:0,template:function(e,m){e&1&&(t(0,"h1"),i(1,"Booking"),a())}})};export{s as BookingPageComponent};
